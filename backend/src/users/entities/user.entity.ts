79547eb3c7e6ad4e0efcc6557c4bffbb86383809
Dániel Czeglédi <132585056+czegi13@users.noreply.github.com>
2026-02-15 12:32:22 +0100

    Added users table

---
+++ backend/src/users/entities/user.entity.ts
@@ -0,0 +1,26 @@
+import { Entity, Column, PrimaryGeneratedColumn} from 'typeorm'
+
+export enum UserRole{
+    ADMIN = 'admin',
+    CUSTOMER = 'customer'
+}
+
+
+@Entity()
+export class User{
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column({unique: true})
+    email: string
+
+    @Column()
+    password: string
+
+    @Column({
+        type: 'enum',
+        enum: UserRole,
+        default: UserRole.CUSTOMER
+    })
+    role: UserRole
+}
